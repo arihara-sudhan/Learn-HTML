941cc75a0824b004266cfe9df94df2d7ce2830fa
Ariharasudhan <100225369+arihara-sudhan@users.noreply.github.com>
2022-09-18 13:29:43 +0530

    Add files via upload

---
+++ Popup1.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+class Popup1 extends {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("The Popup Window!");
+        jf.setSize(1920,1080);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true); }}
